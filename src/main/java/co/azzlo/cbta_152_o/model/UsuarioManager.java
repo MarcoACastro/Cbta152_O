96a2d13ea890b1cb7e6b1a2b731116587e1bd753
Azz <Azz@192.168.1.62>
2016-11-08 23:04:02 -0600

    New Commit

--- src/main/java/co/azzlo/cbta_152_o/model/UsuarioManager.java
+++ src/main/java/co/azzlo/cbta_152_o/model/UsuarioManager.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.azzlo.cbta_152_o.modelo.entidades;
+package co.azzlo.cbta_152_o.model;
 
 import javax.ejb.Stateless;
 
